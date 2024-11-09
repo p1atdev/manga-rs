c6a3de0014692219855dc709891f57886340ad05
Plat <60182057+p1atdev@users.noreply.github.com>
2024-11-10 01:57:29 +0900

    feat: retrieving mangaz episode metadata

---
+++ manga/src/viewer/mangaz/data.rs
@@ -0,0 +1,175 @@
+use std::collections::HashMap;
+use std::fmt;
+
+use anyhow::{bail, Result};
+use chrono::{format, DateTime, Utc};
+use serde::de::{SeqAccess, Visitor};
+use serde::{Deserialize, Deserializer, Serialize};
+use url::Url;
+
+use crate::data::{IndexNumber, MangaEpisode, MangaPage};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct Episode {
+    #[serde(rename = "verkey")]
+    verkey: String,
+    #[serde(rename = "CACHE_VERSION")]
+    cache_version: i64,
+    location: Location,
+    book: Book,
+    authors: HashMap<String, Author>,
+    images: Vec<Option<serde_json::Value>>,
+    #[serde(rename = "Orders")]
+    pages: Vec<Page>,
+    user: User,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Author {
+    id: i64,
+    position: String,
+    name: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Book {
+    baid: i64,
+    series_id: i64,
+    status: String,
+    reading: i64,
+    uploaded_by: Option<serde_json::Value>,
+    title: String,
+    description: Option<String>,
+    site_target: i64,
+    r18: i64,
+    volume: IndexNumber,
+    url: String,
+    thumb_image: String,
+    thumb100: String,
+    thumb160: String,
+    thumb240: String,
+    thumb100_webp: String,
+    thumb160_webp: String,
+    thumb240_webp: String,
+    cover_image: String,
+    publication: String,
+    pdf_sale: i64,
+    vw_share: i64,
+    category: String,
+    rating: i64,
+    premium: i64,
+    pages: i64,
+    trial_pages: Option<serde_json::Value>,
+    #[serde(rename = "Authors")]
+    authors: Vec<String>,
+    page_layout: String,
+    page_direction: i64,
+    page_max_width: i64,
+    page_max_height: i64,
+    image_count: i64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Location {
+    viewer: String,
+    base: String,
+    scramble_dir: String,
+    domain: String,
+    host: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Page {
+    no: i64,
+    name: String,
+    side: Side,
+    pair_no: Option<i64>,
+    scramble: Scramble,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Scramble {
+    w: i64,
+    h: i64,
+    crops: Vec<Crop>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Crop {
+    x: i64,
+    y: i64,
+    x2: i64,
+    y2: i64,
+    w: i64,
+    h: i64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Side {
+    Left,
+    Center,
+    Right,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct User {
+    login: bool,
+    premium: bool,
+    initial: InitialPage,
+    trial: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct InitialPage {
+    image_no: i64,
+}
+
+impl Page {
+    pub fn image_url(&self, base_url: &Url, verkey: &str) -> Result<Url> {
+        let mut url = base_url.join(&self.name)?;
+        url.set_query(Some(format!("{}=", verkey).as_str()));
+        Ok(url)
+    }
+}
+
+impl MangaPage for Page {
+    fn index(&self) -> Result<usize> {
+        Ok(self.no as usize)
+    }
+
+    fn is_image(&self) -> bool {
+        true
+    }
+}
+
+impl Episode {
+    pub fn verkey(&self) -> &str {
+        &self.verkey
+    }
+
+    pub fn image_base_url(&self) -> Result<Url> {
+        let url = Url::parse(&self.location.base)?
+            .join(format!("{}/", &self.location.scramble_dir).as_str())?;
+        Ok(url)
+    }
+}
+
+impl MangaEpisode<Page> for Episode {
+    fn id(&self) -> String {
+        self.book.baid.to_string()
+    }
+
+    fn index(&self) -> IndexNumber {
+        self.book.volume.clone()
+    }
+
+    fn title(&self) -> Option<String> {
+        Some(self.book.title.clone())
+    }
+
+    fn pages(&self) -> Vec<Page> {
+        self.pages.clone()
+    }
+}
---
+++ manga/src/viewer/mangaz/viewer.rs
@@ -0,0 +1,242 @@
+use std::sync::LazyLock;
+
+use anyhow::{anyhow, bail, Result};
+use base64::{engine::general_purpose::STANDARD, Engine as _};
+use regex::Regex;
+use reqwest::header::{self, HeaderMap, HeaderValue};
+use reqwest::Response;
+use scraper::{Html, Selector};
+use url::Url;
+
+use crate::auth::EmptyAuth;
+use crate::utils;
+use crate::viewer::mangaz::data::Episode;
+use crate::viewer::{ViewerClient, ViewerConfig, ViewerConfigBuilder, ViewerWebsite};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Website {
+    MangaZ,
+}
+
+static HOST_TO_WEBSITE: phf::Map<&str, Website> = phf::phf_map! {
+    "www.mangaz.com" => Website::MangaZ,
+    "vw.mangaz.com" => Website::MangaZ,
+};
+
+/// Episode path pattern
+/// - https://vw.mangaz.com/virgo/view/<episode-id>/i:<index>
+/// - https://vw.mangaz.com/navi/<episode-id>
+static EPISODE_PATH_PATTERN: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new(r#"/(navi|virgo/view)/(\d+)"#).unwrap());
+
+impl ViewerWebsite<Website> for Website {
+    fn host(&self) -> &str {
+        match self {
+            Website::MangaZ => "www.mangaz.com",
+        }
+    }
+
+    fn base_url(&self) -> Url {
+        let url = match self {
+            Website::MangaZ => "https://www.mangaz.com",
+        };
+        Url::parse(url).unwrap()
+    }
+
+    fn lookup(host: &str) -> Option<Website> {
+        HOST_TO_WEBSITE.get(host).map(|w| *w)
+    }
+}
+
+impl Website {
+    pub fn viewer_url(&self) -> Url {
+        let url = match self {
+            Website::MangaZ => format!("https://vw.mangaz.com"),
+        };
+        Url::parse(&url).unwrap()
+    }
+}
+
+/// mangaz viewer config
+#[derive(Debug, Clone)]
+pub struct Config {
+    base_url: Url,
+    viewer_url: Url,
+}
+
+impl ViewerConfig for Config {
+    fn create_header(&self) -> Result<HeaderMap> {
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            header::USER_AGENT,
+            HeaderValue::from_str(&utils::UserAgent::Bot.value())?,
+        );
+        headers.insert(
+            header::REFERER,
+            HeaderValue::from_str(&self.base_url.to_string())?,
+        );
+        Ok(headers)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ConfigBuilder {
+    base_url: Url,
+    viewer_url: Url,
+    auth: Option<EmptyAuth>,
+}
+
+impl Default for ConfigBuilder {
+    fn default() -> Self {
+        Self {
+            base_url: Website::MangaZ.base_url(),
+            viewer_url: Website::MangaZ.viewer_url(),
+            auth: None,
+        }
+    }
+}
+
+impl ConfigBuilder {
+    pub fn new(website: Website) -> Self {
+        Self {
+            base_url: website.base_url(),
+            viewer_url: website.viewer_url(),
+            auth: None,
+        }
+    }
+
+    pub fn custom(base_url: String, viewer_url: String) -> Result<Self> {
+        Ok(Self {
+            base_url: Url::parse(&base_url)?,
+            viewer_url: Url::parse(&viewer_url)?,
+            auth: None,
+        })
+    }
+}
+
+impl ViewerConfigBuilder<Config, EmptyAuth> for ConfigBuilder {
+    fn set_auth(&mut self, auth: EmptyAuth) -> &mut Self {
+        self.auth = Some(auth);
+        self
+    }
+
+    fn build(&self) -> Config {
+        Config {
+            base_url: self.base_url.clone(),
+            viewer_url: self.viewer_url.clone(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Client {
+    client: reqwest::Client,
+    config: Config,
+}
+
+impl ViewerClient<Config> for Client {
+    fn new(config: Config) -> Self {
+        let client = reqwest::Client::new();
+        Self { client, config }
+    }
+
+    async fn fetch_raw<B: Into<reqwest::Body> + Send>(
+        &self,
+        url: Url,
+        method: reqwest::Method,
+        body: Option<B>,
+        headers: Option<HeaderMap>,
+    ) -> Result<Response> {
+        let mut req = self
+            .client
+            .request(method, url)
+            .headers(self.config.create_header()?);
+        if let Some(headers) = headers {
+            req = req.headers(headers);
+        }
+        if let Some(body) = body {
+            req = req.body(body);
+        }
+        let res = req.send().await?.error_for_status()?;
+        Ok(res)
+    }
+
+    /// Parse episode id from url
+    fn parse_episode_id(&self, url: &Url) -> Option<String> {
+        let path = url.path();
+        let captures = EPISODE_PATH_PATTERN.captures(path)?;
+        // 1: prefix, 2: episode id, 3: page index
+        captures.get(2).map(|m| m.as_str().to_string())
+    }
+}
+
+impl Client {
+    fn compose_episode_url(&self, episode_id: &str) -> Url {
+        let url = self
+            .config
+            .viewer_url
+            .join(&format!("/virgo/view/{}", episode_id))
+            .unwrap();
+        url
+    }
+
+    fn decode_base64(&self, data: &str) -> Result<String> {
+        let data = STANDARD.decode(data)?;
+        let data = String::from_utf8(data)?;
+        Ok(data)
+    }
+
+    fn extract_data_from_html(&self, html: &str) -> Result<String> {
+        let document = Html::parse_document(html);
+        let selector = match Selector::parse("span#doc") {
+            Ok(selector) => selector,
+            Err(_) => bail!("Failed to parse selector"),
+        };
+        let doc = match document.select(&selector).next() {
+            Some(span) => Ok(span.inner_html()),
+            None => Err(anyhow!("Failed to find span#doc")),
+        }?;
+        Ok(doc.trim().to_string())
+    }
+
+    /// Get episode
+    pub async fn get_episode(&self, episode_id: &str) -> Result<Episode> {
+        let url = self.compose_episode_url(episode_id);
+        let res = self.get(url).await?;
+        let html = res.text().await?;
+        let encoded = self.extract_data_from_html(&html)?;
+        let decoded = self.decode_base64(&encoded)?;
+        let episode = serde_json::from_str(&decoded)?;
+        Ok(episode)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use crate::data::{MangaEpisode, MangaPage};
+
+    #[tokio::test]
+    async fn test_get_episode() {
+        let episode_ids = vec!["211991", "139231", "65031"];
+
+        for &episode_id in episode_ids.iter() {
+            let config = ConfigBuilder::new(Website::MangaZ).build();
+            let client = Client::new(config);
+            let episode = client.get_episode(episode_id).await.unwrap();
+            assert_eq!(episode.id(), episode_id);
+            assert!(episode.title().is_some());
+
+            let page = episode.pages();
+            let base_url = episode.image_base_url().unwrap();
+            let verkey = episode.verkey();
+
+            for p in page {
+                let index = p.index().unwrap();
+                let url = p.image_url(&base_url, &verkey).unwrap();
+                println!("{}: {}", index, url);
+            }
+        }
+    }
+}
