22b7e2dd0698539d0ddaca14e4c4147b2f27a656
Plat <60182057+p1atdev@users.noreply.github.com>
2024-12-24 19:57:01 +0900

    chore: add empty solver that does nothing

--- manga/src/solver.rs
+++ manga/src/solver.rs
@@ -10,3 +10,16 @@ pub trait ImageSolver {
     /// Solve the obfuscated bytes to an image.
     fn solve_from_bytes<B: AsRef<[u8]>>(&self, bytes: B) -> Result<DynamicImage>;
 }
+
+/// An empty solver that does nothing.
+pub struct EmptySolver;
+
+impl ImageSolver for EmptySolver {
+    fn solve<T: AsRef<[u8]>>(&self, bytes: T) -> Result<Bytes> {
+        Ok(bytes.as_ref().to_vec())
+    }
+
+    fn solve_from_bytes<B: AsRef<[u8]>>(&self, bytes: B) -> Result<DynamicImage> {
+        Ok(image::load_from_memory(bytes.as_ref())?)
+    }
+}
